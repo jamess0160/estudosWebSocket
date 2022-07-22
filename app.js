9d661d87c0938fbdb23d0d9b737efa4c66789db2
Tiago Ribeiro <tiagor@btiestrategica.com.br>
2022-07-22 09:52:35 -0300

    atualizações de layout e usuarios

--- app.js
+++ app.js
@@ -10,21 +10,41 @@ const socket = socketIO(server)
 app.use(express.static('./public'))
 
 let mensagens = []
+let pessoasConectadas = []
 
 socket.on('connection', (stream) => {
-    stream.emit('attMensagem', mensagens)
+	var nomeStream;
+	pessoasConectadas.push(stream.id)
+	socket.emit("nomes", pessoasConectadas)
 
-    stream.on("msg", recebe => {
-        mensagens.push({ origem: recebe.nome || stream.id, msg: recebe.msg, hora: recebe.hora })
-        socket.emit('attMensagem', mensagens)
-    })
+	console.log(new Date(), ">> nova conexão:", stream.id)
+	stream.emit('attMensagem', mensagens)
+
+	stream.on("msg", payload => {
+		mensagens.push({ origem: payload.nome || stream.id, msg: payload.msg, hora: payload.hora })
+		if(payload.msg == "/clear"){
+			mensagens = []
+		}
+		socket.emit('attMensagem', mensagens)
+	})
+
+	stream.on("attName", (payload)=>{
+		nomeStream = payload.nome
+		let index = pessoasConectadas.indexOf(stream.id)
+		pessoasConectadas[index] = payload.nome
+		socket.emit("nomes", pessoasConectadas)
+	})
+
+	stream.on("disconnect", ()=>{
+		let indexNome = pessoasConectadas.indexOf(nomeStream)
+		pessoasConectadas[indexNome] = null
+		let indexID = pessoasConectadas.indexOf(stream.id)
+		pessoasConectadas[indexID] = null
+		pessoasConectadas = pessoasConectadas.filter(item=> item !== null)
+		socket.emit("nomes", pessoasConectadas)
+	})
 })
-setTimeout(()=>{
-    socket.emit('atualizou', true)
-}, 1000)
 
-
-
-server.listen(4000, () => {
-    console.log("servidor iniciado em: http://localhost:4000")
+server.listen(4001, () => {
+	console.log("servidor iniciado em: http://localhost:4001")
 })
